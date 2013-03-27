aa3aa259d6ac6a101edb8da4a1550b3d0233f801
Albert Latacz <albert.latacz@gmail.com>
2013-03-27 12:11:03 +0000

    publish app and latest

--- bin/push.sh
+++ bin/push.sh
@@ -3,6 +3,9 @@
 echo -e "[default]\naccess_key = $AWS_KEY\nsecret_key = $AWS_SECRET" > ~/.s3cfg
 
 s3cmd -c ~/.s3cfg put build/artifacts/javarepl-intellij-$JAVAREPL_BUILD_NUMBER.zip s3://albertlatacz.published/javarepl/intellij-plugin/javarepl-intellij-$JAVAREPL_BUILD_NUMBER.zip
+s3cmd -c ~/.s3cfg put build/artifacts/javarepl-intellij-$JAVAREPL_BUILD_NUMBER.zip s3://albertlatacz.published/javarepl/intellij-plugin/javarepl-intellij-latest.zip
+
 s3cmd -c ~/.s3cfg put build/artifacts/javarepl-$JAVAREPL_BUILD_NUMBER.jar s3://albertlatacz.published/javarepl/javarepl-$JAVAREPL_BUILD_NUMBER.jar
+s3cmd -c ~/.s3cfg put build/artifacts/javarepl-$JAVAREPL_BUILD_NUMBER.jar s3://albertlatacz.published/javarepl/javarepl-latest.jar
 
 rm ~/.s3cfg
