5dd89a703ac04f1a0a806da03ab1fe71ee71af9b
Albert Latacz <albert.latacz@gmail.com>
2013-03-27 10:30:17 +0000

    aws secures

--- bin/push.sh
+++ bin/push.sh
@@ -2,6 +2,6 @@
 
 echo -e "[default]\naccess_key = $AWS_KEY\nsecret_key = $AWS_SECRET" > ~/.s3cfg
 
-s3cmd put build/artifacts/javarepl-intellij-dev.build.zip s3://albertlatacz.published/javarepl-intellij-dev.build.zip
+s3cmd -c .s3cfg put build/artifacts/javarepl-intellij-dev.build.zip s3://albertlatacz.published/javarepl-intellij-dev.build.zip
 
 rm ~/.s3cfg
