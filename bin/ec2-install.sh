1a502870e9a7ba1d94280fbb04dc90e3c94dc380
Albert Latacz <albert.latacz@gmail.com>
2013-04-24 18:35:10 +0100

    release to ec2 script

---
+++ bin/ec2-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Installing java-repl..."
+
+wget 'http://albertlatacz.published.s3.amazonaws.com/javarepl/javarepl.jar' -O /home/ubuntu/javarepl.jar
+
+nohup java -jar /home/ubuntu/javarepl.jar --simple-console --sandboxed --port=8001 2>&1 > javarepl.out &
