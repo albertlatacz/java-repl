502a542fd06b86563cd5e16ebaa37f49e97e299f
Albert Latacz <albert.latacz@gmail.com>
2013-04-24 09:03:08 +0100

    removed EvaluatorTestHelper and added test for shadowing

--- test/javarepl/EvaluatorTest.java
+++ test/javarepl/EvaluatorTest.java
@@ -1,10 +1,14 @@
 package javarepl;
 
+import com.googlecode.totallylazy.Either;
 import com.googlecode.totallylazy.Option;
 import javarepl.expressions.*;
+import org.hamcrest.FeatureMatcher;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
-import static javarepl.EvaluationTestHelper.*;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -38,10 +42,60 @@ public class EvaluatorTest {
     }
 
     @Test
+    public void shouldShadowPreviousResult() {
+        assertThat(evaluating("test = 21", "test = 42", "test"), hasResult(42));
+    }
+
+    @Test
     public void shouldReturnTypeOfExpression() {
         assertThat(new Evaluator().typeOfExpression("\"hello\""), is(Option.<Class>some(String.class)));
         assertThat(new Evaluator().typeOfExpression("12"), is(Option.<Class>some(Integer.class)));
         assertThat(new Evaluator().typeOfExpression("System.out.println(\"hello\")"), is(Option.<Class>none()));
+    }
+
+
+    private static Matcher<Either<? extends Throwable, Evaluation>> hasNoResult() {
+        return new FeatureMatcher<Either<? extends Throwable, Evaluation>, Object>(Matchers.<Object>is(Result.noResult()), "result value", "result value") {
+            protected Object featureValueOf(Either<? extends Throwable, Evaluation> evaluation) {
+                return evaluation.right().result();
+            }
+        };
+    }
+
+    private static Matcher<Either<? extends Throwable, Evaluation>> hasCompilationError() {
+        return new FeatureMatcher<Either<? extends Throwable, Evaluation>, Object>(Matchers.<Object>is(instanceOf(ExpressionCompilationException.class)), "result value", "result value") {
+            protected Throwable featureValueOf(Either<? extends Throwable, Evaluation> evaluation) {
+                return evaluation.left();
+            }
+        };
+    }
+
+    private static <T> Matcher<Either<? extends Throwable, Evaluation>> hasResult(T value) {
+        return new FeatureMatcher<Either<? extends Throwable, Evaluation>, T>(is(value), "result value", "result value") {
+            protected T featureValueOf(Either<? extends Throwable, Evaluation> evaluation) {
+                return (T) evaluation.right().result().get().value();
+            }
+        };
+    }
+
+
+    private static Matcher<Either<? extends Throwable, Evaluation>> hasExpressionOfType(Class<?> clazz) {
+        return new FeatureMatcher<Either<? extends Throwable, Evaluation>, Expression>(instanceOf(clazz), "result value", "result value") {
+            protected Expression featureValueOf(Either<? extends Throwable, Evaluation> evaluation) {
+                return evaluation.right().expression();
+            }
+        };
+    }
+
+    private static Either<? extends Throwable, Evaluation> evaluating(String... expressions) {
+        Evaluator evaluator = new Evaluator();
+
+        Either<? extends Throwable, Evaluation> result = null;
+        for (String expression : expressions) {
+            result = evaluator.evaluate(expression);
+        }
+
+        return result;
     }
 
 }
